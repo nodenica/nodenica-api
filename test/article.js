34da6cacff69f5778b52b3d9f4abdc6c233f0ebb
Paulo McNally <paulomcnally@gmail.com>
2014-10-25 19:53:42 -0600

    CRUD Unit testing added

---
+++ test/article.js
@@ -0,0 +1,106 @@
+require('should-http');
+var assert = require('assert');
+var Settings = require('./settings.conf');
+var settings = new Settings();
+
+describe('Article', function() {
+
+  var form = {
+    'title': 'Unit Test ' + settings.getRandomString(),
+    'content': 'This is a Unit Test',
+    'type_id': 1
+  };
+
+  var article = settings.http.post('Articles', form, settings.auth.foo.accessToken);
+  var articleObj = JSON.parse(article.body);
+
+  describe('Create', function() {
+    it('Foo create', function() {
+      article.should.have.status(200);
+    });
+  });
+
+  describe('Read', function() {
+    it('ALLOW find to Admin A', function() {
+      var getRequest = settings.http.get('Articles/' + articleObj.id, settings.auth.adminA.accessToken);
+      getRequest.should.have.status(200);
+    });
+    it('ALLOW find to Admin B', function() {
+      var getRequest = settings.http.get('Articles/' + articleObj.id, settings.auth.adminB.accessToken);
+      getRequest.should.have.status(200);
+    });
+    it('ALLOW find to Admin C', function() {
+      var getRequest = settings.http.get('Articles/' + articleObj.id, settings.auth.adminC.accessToken);
+      getRequest.should.have.status(200);
+    });
+    it('ALLOW find to Owner (Foo)', function() {
+      var getRequest = settings.http.get('Articles/' + articleObj.id, settings.auth.foo.accessToken);
+      getRequest.should.have.status(200);
+    });
+    it('ALLOW find to Bar', function() {
+      var getRequest = settings.http.get('Articles/' + articleObj.id, settings.auth.bar.accessToken);
+      getRequest.should.have.status(200);
+    });
+  });
+
+  describe('Update', function() {
+    it('ALLOW updateAttributes to Admin A', function() {
+      var form = {
+        title: 'Edited Admin A ' + settings.getRandomString()
+      };
+      var editRequest = settings.http.put('Articles/' + articleObj.id, form, settings.auth.adminA.accessToken);
+      editRequest.should.have.status(200);
+    });
+    it('ALLOW updateAttributes to Admin B', function() {
+      var form = {
+        title: 'Edited Admin B ' + settings.getRandomString()
+      };
+      var editRequest = settings.http.put('Articles/' + articleObj.id, form, settings.auth.adminB.accessToken);
+      editRequest.should.have.status(200);
+    });
+    it('ALLOW updateAttributes to Admin C', function() {
+      var form = {
+        title: 'Edited Admin C ' + settings.getRandomString()
+      };
+      var editRequest = settings.http.put('Articles/' + articleObj.id, form, settings.auth.adminC.accessToken);
+      editRequest.should.have.status(200);
+    });
+    it('ALLOW updateAttributes to Owner (Foo)', function() {
+      var form = {
+        title: 'Edited Owner ' + settings.getRandomString()
+      };
+      var editRequest = settings.http.put('Articles/' + articleObj.id, form, settings.auth.foo.accessToken);
+      editRequest.should.have.status(200);
+    });
+    it('DENY updateAttributes to Bar', function() {
+      var form = {
+        title: 'Edited Bar ' + settings.getRandomString()
+      };
+      var editRequest = settings.http.put('Articles/' + articleObj.id, form, settings.auth.bar.accessToken);
+      editRequest.should.have.status(401);
+    });
+  });
+
+  describe('Delete', function() {
+    it('DENY deleteById to Bar', function() {
+      var deleteRequest = settings.http.delete('Articles/' + articleObj.id, settings.auth.bar.accessToken);
+      deleteRequest.should.have.status(401);
+    });
+    it('ALLOW deleteById to Owner (Foo)', function() {
+      var deleteRequest = settings.http.delete('Articles/' + articleObj.id, settings.auth.foo.accessToken);
+      deleteRequest.should.have.status(204);
+    });
+    it('ALLOW deleteById to Admin A', function() {
+      var deleteRequest = settings.http.delete('Articles/' + articleObj.id, settings.auth.adminA.accessToken);
+      deleteRequest.should.have.status(204);
+    });
+    it('ALLOW deleteById to Admin B', function() {
+      var deleteRequest = settings.http.delete('Articles/' + articleObj.id, settings.auth.adminB.accessToken);
+      deleteRequest.should.have.status(204);
+    });
+    it('ALLOW deleteById to Admin A', function() {
+      var deleteRequest = settings.http.delete('Articles/' + articleObj.id, settings.auth.adminC.accessToken);
+      deleteRequest.should.have.status(204);
+    });
+  });
+});
