ff51775bc535a68e850260e936efb59b018cf94f
Paulo McNally <paulomcnally@gmail.com>
2014-10-26 11:58:32 -0600

    added hook to model type

--- common/models/type.js
+++ common/models/type.js
@@ -1,3 +1,18 @@
 module.exports = function(Type) {
+  Type.beforeRemote('create', function(ctx, type, next) {
+    Type.findOne({where: {name: ctx.req.body.name}}, function(err, result) {
+      if (err) {
+        next(new Error(err));
+      }
+      else if (result !== null) {
+        var err = new Error('Ya se ha definido este tipo.');
+        err.statusCode = 409;
+        next(err);
+      }
+      else {
+        next();
+      }
+    });
+  });
 
 };
